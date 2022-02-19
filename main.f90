5fbda53e171427e8de26df00e473a13251f99acf
Callum Hughes <callumhughes2000@live.co.uk>
2022-02-19 21:11:36 +0000

    Added function to calculate value of Chi without coefficients. Also compiled programme to produce an executable.

--- main.f90
+++ main.f90
@@ -1,18 +1,32 @@
 program main
-implicit none
-real, parameter :: dp=selected_real_kind(15,300)
+  implicit none
+  integer, parameter :: dp=selected_real_kind(15,300)
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!! DEFINE OBJECTS !!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-integer :: l
-real (kind=dp) :: E, V, r, delta
+  integer :: l
+  real (kind=dp) :: Energy, Potential, radius,  delta
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!! MAIN PROGRAMME !!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+  Energy = 8
+  radius = 2
+
+  print *, Chi(Energy,radius)
+
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!! FUNCTIONS AND SUBROUTINES !!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+contains
+  function Chi(E,r)
+    !!! Function to calculate the value for Chi (What is chi) !!!
+    real (kind=dp) :: Chi, E, r
+
+    Chi = sin(sqrt(2*E)*r)
+  end function
+
+end program
