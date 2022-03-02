45299f2ae4ebb7e8c7df1423d0f6ec27e9750b70
Callum Hughes <callumhughes2000@live.co.uk>
2022-03-02 17:25:18 +0000

    Added function R to calculate traial wavefunction using radius.

--- main.f90
+++ main.f90
@@ -6,17 +6,20 @@ program main
 !!!!!!!!!!!! DEFINE OBJECTS !!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  integer :: l
+  integer :: l, n
   real (kind=dp) :: Energy, Potential, radius,  delta
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!! MAIN PROGRAMME !!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  Energy = 8
-  radius = 2
+  !Energy = 8
+  radius = 1.0
+  n = 1
 
-  print *, Chi(Energy,radius)
+  print *, R(radius,n)
+
+  !print *, Chi(Energy,radius)
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!! FUNCTIONS AND SUBROUTINES !!!!!!!
@@ -27,6 +30,19 @@ contains
     real (kind=dp) :: Chi, E, r
 
     Chi = sin(sqrt(2*E)*r)
+  end function
+
+  function R(radius,n)
+    !!! Function to to define trial wavefunction !!!
+    real (kind=dp), parameter :: R0 = 3.0
+    real (kind=dp) :: R, radius
+    integer :: n
+
+    if (radius .LE. R0) then
+      R = 1 - (radius/R0)**n
+    else
+      R = 0
+    end if
   end function
 
 end program
